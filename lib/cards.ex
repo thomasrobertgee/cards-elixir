26cd3f93c929364e418e8008513c694825997cf7
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-07 13:27:33 +1100

    can save and load decks, clean error displays when file not found

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,9 +3,11 @@ defmodule Cards do # module
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits do
-      suit
+    for suit <- suits, value <- values do # multiple comprehensions
+        "#{value} of #{suit}"
     end
+
+
   end
 
   def shuffle(deck) do # passing an arguement to a method (same as any other langauge)
@@ -14,5 +16,23 @@ defmodule Cards do # module
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck) # check out docs for erlang
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "That file doesn not exist"
+    end
   end
 end
