14a96b8fd2fc5516bcfef024eda2425962c961e1
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-07 11:25:35 +1100

    learning basics of Elixir with a few simple methods, understanding the difference between OOP and functional, continuing on to more complex methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,3 +1,18 @@
-defmodule Cards do
-  
+defmodule Cards do # module
+  def create_deck do # method
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits do
+      suit
+    end
+  end
+
+  def shuffle(deck) do # passing an arguement to a method (same as any other langauge)
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 end
